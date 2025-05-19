531a55e2bf336a17e6d03ddb6a0c8ff33de37b2d
beestcode <153935496+beestcode@users.noreply.github.com>
2025-05-19 14:46:19 +0500

    can add values by replacing the initial zero

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ document.addEventListener('DOMContentLoaded', function() {
     let display = document.getElementById('Display');
     display.value = '0';
 });
+
+function writevalue(value) {
+    value = value === '×' ? '*' : value === '÷' ? '/' : value;
+    let display = document.getElementById('Display');
+    let operators = ['+', '-', '*', '/'];
+
+    if (display.value === '0' ? (display.value = value) : (display.value += value)){
+        return;
+    }
+    
+    
+ 
+}
