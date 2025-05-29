be5de33843ade7c2364a3808cecd8159f029aaee
beestcode <153935496+beestcode@users.noreply.github.com>
2025-05-29 23:01:46 +0500

    calculate running seccessfuly

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     let display = document.getElementById('Display');
+    display.style.color = '';
     display.value = '0';
 });
 
 function writevalue(value) {
     value = value === '×' ? '*' : value === '÷' ? '/' : value;
     let display = document.getElementById('Display');
+    display.style.color = '';
+
     let last = display.value.slice(-1)
     let operators = ['+', '-', '*', '/'];
 
@@ -26,15 +29,35 @@ function writevalue(value) {
 
 function remove() {
     let display = document.getElementById('Display')
+    display.style.color = '';
 
     if (display.value.length > 1 ? display.value = display.value.slice(0, -1) : display.value = '0');
 }
 
 function clearall() {
-    document.getElementById('Display').value = '0'
+    display = document.getElementById('Display')
+    display.style.color = '';
+    
+    display.value = '0';
 }
 
 function clearentry() {
     let display = document.getElementById('Display')
+    display.style.color = '';
+
     display.value = display.value.replace(/[\d.]+$/, "") || '0';
+}
+
+function calculate() {
+    let display = document.getElementById('Display');
+    if (display.value.trim() === '') return;
+
+    try {
+        display.value = Function("return " + display.value)();
+        display.style.color = ''; // Reset to default color
+    }
+    catch (e) {
+        display.value = 'Error';
+        display.style.color = 'red';
+    }
 }
